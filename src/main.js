1f1648d2ab0a8859434f396d430fb3bd2e02dafc
Lucas GS <chambe89@gmail.com>
2022-07-14 19:25:19 +0200

    Independent address list

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import i18n from '@/modules/address-form/locales/i18n'
-import store from './store'
+import App1 from './App1.vue'
+import App2 from './App2.vue'
+import i18nAddressForm from '@/modules/address-form/locales/i18n'
+import i18nAddressList from '@/modules/address-list/locales/i18n'
+import { store1, store2 } from './store'
 
 
-createApp(App)
-    .use(store)
-    .use(i18n)
-    .mount('#app')
+createApp(App1)
+    .use(store1)
+    .use(i18nAddressForm)
+    .mount('#app1')
+
+
+createApp(App2)
+    .use(store2)
+    .use(i18nAddressList)
+    .mount('#app2')
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,17 @@
 import { createStore } from 'vuex'
 
 import addressForm from '@/modules/address-form/store'
+import addressList from '@/modules/address-list/store'
 
-const store = createStore({
+export const store1 = createStore({
     modules: {
         addressForm
     }
 })
 
+export const store2 = createStore({
+    modules: {
+        addressList
+    }
+})
 
-export default store
