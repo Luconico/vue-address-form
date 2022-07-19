c1837b8ea83970d5e7c1f4075d36005bf8a8a516
Lucas GS <chambe89@gmail.com>
2022-07-19 09:48:05 +0200

    Changing address form

---
+++ src/shared/buttons/Button.vue
@@ -0,0 +1,20 @@
+<template>
+      <button
+      class="btn btn-secondary"
+      :disabled="country.length < 1 || isDisabled"
+    >
+      <LoaderSpinner v-if="isFetching" color="gray" size="1rem" />{{
+        $t(`buttons.${buttonText}`)
+      }}
+    </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
