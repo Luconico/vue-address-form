4662b1cc0aae815b49723c1df884ebbaed815b26
Lucas GS <chambe89@gmail.com>
2022-07-19 08:48:42 +0200

    Adding AlertMessages in shared

---
+++ src/shared/alert-message/AlertMessages.vue
@@ -0,0 +1,27 @@
+<template>
+    <AlertMessage
+    v-for="(message, key) in messages"
+    :key="key"
+    :message="message"
+  />
+</template>
+
+<script>
+import AlertMessage from "./AlertMessage.vue";
+export default {
+  name: "alert-messages",
+    components: {
+    AlertMessage
+  },
+  props: {
+    messages: {
+        type: Array,
+        default: []
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/shared/forms/form-builder/FormBuilder.vue
@@ -0,0 +1,20 @@
+<template>
+  <AlertMessages :messages="messages"  />
+
+  <form id="address-form" class="custom-form" @submit.prevent="onSubmit">
+
+  </form>
+</template>
+
+<script>
+import AlertMessages from "@/shared/alert-message/AlertMessage.vue";
+export default {
+  name: "form-builder",
+  components: {
+    AlertMessages
+  },
+};
+</script>
+
+<style>
+</style>
